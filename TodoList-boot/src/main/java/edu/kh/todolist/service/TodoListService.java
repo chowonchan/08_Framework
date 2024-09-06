5e748abc75a62989bf73ee33113337a25fe3750c
Chowonchan <cwc2264@gmail.com>
2024-09-06 17:46:25 +0900

    2024 0906 todoList Spring (mapper,config, mybatis)

---
+++ TodoList-boot/src/main/java/edu/kh/todolist/service/TodoListService.java
@@ -0,0 +1,12 @@
+package edu.kh.todolist.service;
+
+import java.util.Map;
+
+public interface TodoListService {
+
+	/** 할일 목록 조회 + 완료된 할일 개수
+	 * @return
+	 */
+	Map<String, Object> selectTodoList();
+
+}
